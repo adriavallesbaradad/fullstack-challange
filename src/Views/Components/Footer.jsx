e19daa9c1304f40af8cecefbf5167b106d4fbd18
Adrià Vallès Baradad <100840351+adriavallesbaradad@users.noreply.github.com>
2022-11-04 23:13:51 +0100

    last

--- src/Views/Components/Footer.jsx
+++ src/Views/Components/Footer.jsx
@@ -73,9 +73,9 @@ const navigation = {
   
   export default function Example() {
     return (
-      <footer className="flex flex-col min-h-screen mt-12 bg-gray-800">
-        <div className="mx-auto max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
-          <nav className="-mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
+      <footer className="flex flex-col  bg-gray-800">
+        <div className=" max-w-7xl overflow-hidden py-12 px-4 sm:px-6 lg:px-8">
+          <nav className="mx-5 -my-2 flex flex-wrap justify-center" aria-label="Footer">
             {navigation.main.map((item) => (
               <div key={item.name} className="px-5 py-2">
                 <a href={item.href} className="text-base text-gray-500 hover:text-gray-900">
